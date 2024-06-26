674ad81b55e5298df79aabae400f899a54fca366
1karu32s <2819900370@qq.com>
2024-06-26 17:31:50 +0800

    增加了新增员工的功能，已通过线程存储空间解决了获取请求id的内容

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -3,6 +3,7 @@ package com.sky.service.impl;
 import com.sky.constant.MessageConstant;
 import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
+import com.sky.context.BaseContext;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
@@ -73,9 +74,9 @@ public class EmployeeServiceImpl implements EmployeeService {
 //        设置当前系统时间
         employee.setCreateTime(LocalDateTime.now());
         employee.setUpdateTime(LocalDateTime.now());
-//TODO 后期需要修改
-        employee.setCreateUser(10L);
-        employee.setUpdateUser(10L);
+//TODO 后期需要修改(已通过线程存储空间解决)
+        employee.setCreateUser(BaseContext.getCurrentId());
+        employee.setUpdateUser(BaseContext.getCurrentId());
 
         employeeMapper.insert(employee);
     }
